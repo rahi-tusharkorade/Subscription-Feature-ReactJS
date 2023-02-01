f514f18c8dac8676f66e9dde81bd78ddd7c08cbb
Tushar <tushar.korade@rahisystems.com>
2023-02-01 11:19:11 +0530

    update popup

--- Subscribe.jsx
+++ Subscribe.jsx
@@ -6,17 +6,33 @@ import Popup from 'reactjs-popup';
 import React from 'react';
 import 'reactjs-popup/dist/index.css';
 
-function mysubscription ( props ) {
-    const[data,setdata]= useState([]);
+function mysubscription(props) {
+    const [data, setdata] = useState([]);
     const [isShow, setIsShow] = useState(false)
+    const [popdata, setpopdata] = useState([])
 
-    useEffect( () => {
-        setdata(props.sublst.map(d => ({...d, status: "Subscribe"})));
-        // console.log("item codeeeeeeeeeeeeeee",props.sublst
-       }, [ ] )
-       
+    useEffect(() => {
+        setdata(props.sublst.map(d => ({ ...d, status: "Subscribe" })));
+    }, [])
+
     return (
         <div className="main">
+            
+            {
+            isShow &&
+                <Popup
+                    open={isShow}
+                    modal nested>
+                    <div className='newpopup'>
+                        <span ><b>Product Details</b>
+                            <button className="close" onClick={() => setIsShow(false)}>x</button>
+                            <br /><br />
+                        </span><b>Itemcode - </b> {popdata.itemcode} <br /> <br />
+                        <b>Description - </b>{popdata.description.replace(/(<([^>]+)>)/ig, '')}
+                    </div>
+                </Popup>
+            }
+            
             <PageHeader title="My Subscriptions" subTitle="Account Information" />
             <nav className="breadcrumb-nav">
                 <div className="container">
@@ -38,75 +54,71 @@ function mysubscription ( props ) {
 
                         <div>
                             <div className="row">
-                                        <div className="subscribe-rahimyorder-col-sm-6">
-                                            <h3 >My Subscriptions</h3>
-                                            <hr className="mt-0"></hr>
-                                            <br></br>
-                                            <div id="table-wrapper" >
-                                                <div className="table-scroll fixTableHead">
-                                                    <table className="custom_info data" id="myTable">
-                                                        <thead>
-                                                            <tr >
-                                                                <th scope="col" className="rahi-col id">Subscription Id</th>
-                                                                <th scope="col" className="rahi-col date">Purchase Date</th>
-                                                                <th scope="col" className="rahi-col shipping">Next Purchase Date</th>
-                                                                <th scope="col" className="rahi-col status">Status</th>
-                                                                <th scope="col" className="rahicol actions">Action</th>
-                                                            </tr>
-                                                        </thead>
-                                                        <tbody>
-                                                            {
-                                                                data.map( ( d, index ) =>(
-                                                                 <>
-                                                                    <tr className="parent" key={index}>
-                                                                        <td className='rahi-down-caret'>{d['itemcode']}</td>
-                                                                        <td className='rahi-down-table'> {Moment(d['Transaction Date']).format('MM/DD/YYYY')}</td>
-                                                                        <td className='rahi-down-table'>{props.subdate}</td>
-                                                                        <td className='rahi-down-table' >{d.status}</td>
-                                                                        <td data-th="Actions" className="rahi-col actions rahi-down-table">
-                                                                            {/* <button className="action view" onClick={props.subformshow}><span>View </span>  |</button>  */}
-
-                                                                                <div>  
-                                                                                    {/* <button onClick={(e) =>{  setIsShow(true)}} > View </button> */}
-                                                                                    <Popup trigger={<button onClick={(e) =>{  setIsShow(true)}} > View </button>}
-                                                                                        open={isShow} 
-                                                                                        modal nested>
-                                                                                        <div  className='newpopup'>
-                                                                                            <span ><b>Product Details</b>
-                                                                                            <br/><br/>
-                                                                                            </span><b>Itemcode - </b>{d["itemcode"]} <br /> <br/>
-                                                                                            <b>Description - </b>{d["description"].replace(/(<([^>]+)>)/ig, '')}
-                                                                                        </div>
-                                                                                    </Popup>
-                                                                                </div> 
-                                                                                
-                                                                            <span>|</span>
-                                                                            <button className="action view"  onClick={(e) => {
-                                                                                var temp = data[index];
-                                                                                var newArray = data.map(dt => {
-                                                                                    if(dt.itemcode === temp.itemcode){
-                                                                                        return {
-                                                                                            ...temp,
-                                                                                            status: "Unsubscribe",
-                                                                                        }
-                                                                                    }else{
-                                                                                        return dt
+                                <div className="subscribe-rahimyorder-col-sm-6">
+                                    <h3 >My Subscriptions</h3>
+                                    <hr className="mt-0"></hr>
+                                    <br></br>
+                                    <div id="table-wrapper" >
+                                        <div className="table-scroll fixTableHead">
+                                            <table className="custom_info data" id="myTable">
+                                                <thead>
+                                                    <tr >
+                                                        <th scope="col" className="rahi-col id">Subscription Id</th>
+                                                        <th scope="col" className="rahi-col date">Purchase Date</th>
+                                                        <th scope="col" className="rahi-col shipping">Next Purchase Date</th>
+                                                        <th scope="col" className="rahi-col status">Status</th>
+                                                        <th scope="col" className="rahicol actions">Action</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    {
+                                                        data.map((d, index) => (
+                                                            <>
+                                                                <tr className="parent" key={index}>
+                                                                    <td className='rahi-down-caret'>{d['itemcode']}</td>
+                                                                    <td className='rahi-down-table'> {Moment(d['Transaction Date']).format('MM/DD/YYYY')}</td>
+                                                                    <td className='rahi-down-table'>{props.subdate}</td>
+                                                                    <td className='rahi-down-table' >{d.status}</td>
+                                                                    <td data-th="Actions" className="rahi-col actions rahi-down-table">
+                                                                        {/* <button className="action view" onClick={props.subformshow}><span>View </span>  |</button>  */}
+                                                                        <div>
+                                                                            <button name={d["itemcode"]} value={d["description"]} onClick={() =>
+                                                                                {
+                                                                                    setpopdata(data[index])
+                                                                                    // console.log(popdata)
+                                                                                    setIsShow(true)
+                                                                                }}
+                                                                            >
+                                                                                <span>View</span> 
+                                                                            </button>
+                                                                        </div>
+                                                                        <span>|</span>
+                                                                        <button className="action view" onClick={(e) => {
+                                                                            var temp = data[index];
+                                                                            var newArray = data.map(dt => {
+                                                                                if (dt.itemcode === temp.itemcode) {
+                                                                                    return {
+                                                                                        ...temp,
+                                                                                        status: "Unsubscribe",
                                                                                     }
-                                                                                })
-                                                                                setdata(newArray)
-                                                                                
-                                                                            }}><span> Cancel Subscription</span></button>
-                                                                        </td>
-                                                                    </tr>
-                                                                    
-                                                                </>
-                                                            ))}
-                                                        </tbody>
-                                                    </table>
-                                                </div>
-                                            </div>
-                                        </div>                                
+                                                                                } else {
+                                                                                    return dt
+                                                                                }
+                                                                            })
+                                                                            setdata(newArray)
+
+                                                                        }}><span> Cancel Subscription</span></button>
+                                                                    </td>
+                                                                </tr>
+
+                                                            </>
+                                                        ))}
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
                                 </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -114,4 +126,4 @@ function mysubscription ( props ) {
         </div>
     )
 }
-export default  mysubscription ;
+export default mysubscription;
